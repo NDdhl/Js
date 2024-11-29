1219605b7b988167774cd37604d7dc55ccbee459
NDdhl <144937518+NDdhl@users.noreply.github.com>
2024-11-29 08:08:58 +0700

    Update doi_gio_phut.js

--- js/doi_gio_phut.js
+++ js/doi_gio_phut.js
@@ -1,9 +1,9 @@
-function time_convert(num)
+function time_convert(num)  // cho phút tính giờ
  { 
-  var hours = Math.floor(num / 60);  //làm tròn
-
+  var hours = Math.floor(num / 60);  
+  //Math.floor: trả  vềsố nguyên lớn nhất nhỏ hơn hoặc bằng
   var minutes = num % 60;
-
+  //chia lấy sư 
   return hours + ":" + minutes;         
 }
 
