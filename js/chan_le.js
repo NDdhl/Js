81b5c66b49ce6bde6b7f1de0c60a38927bc17940
NDdhl <144937518+NDdhl@users.noreply.github.com>
2024-11-29 08:02:17 +0700

    Update chan_le.js

--- js/chan_le.js
+++ js/chan_le.js
@@ -1,14 +1,14 @@
-for (var x = 0; x <= 15; x++) 
+for (var x = 0; x <= 15; x++) /* Kiêm tra chẳn lẻ của các số từ 0-15 */
 {
-    if (x === 0) 
+    if (x === 0)     /* === so sánh bằng: giá trị+kiểu dữ liệu */
     {
-        console.log(x + " is even");
+        console.log(x + " là số chẵn");
     }
-    else if (x % 2 === 0) 
+    else if (x % 2 === 0)       /* x chia 2 có phần dư*/
     {
-        console.log(x + " is even");
+        console.log(x + " là số chẵn");
     }
     else {
-        console.log(x + " is odd");
+        console.log(x + " là số lẻ");
     }
 } 
